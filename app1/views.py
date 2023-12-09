53cf00ecd96c12acf98918a4630b7a05608e020f
Mounika9390 <mmounika9390@gmail.com>
2023-12-09 22:46:54 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mounika(request):
+    return render(request,'mounika.html')
---
+++ app2/urls.py
@@ -0,0 +1,6 @@
+from app2.views import *
+from django.urls import path
+app_name='AI2'
+urlpatterns=[
+    path('sindhu/',sindhu,name='sindhu'),
+]
